3c725c310ab1e5dcc385285958c46dede1506f2d
isharpz007 <sharpz7575@gmail.com>
2026-08-21 09:53:24 +0100

    UPDATED 001

--- odi/backend/main.py
+++ odi/backend/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 def root():
     return {"message": "OdiAI backend is running"}
+
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello from Python FastAPI!"}
